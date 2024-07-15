84c1f2da51b2da9e2b1982379dd0db71bed62ef4
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-07-15 12:35:34 -0400

    finished palyer details and return to roster button

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO -
+// style with css
 // Observe a player's details.
 
 
@@ -8,11 +9,11 @@
 
 const state = {}
 
-const baseAPI = "https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft/players";
+const api = "https://fsa-puppy-bowl.herokuapp.com/api/2406-ftb-et-web-ft/players";
 
 const getPlayers = async () => {
   try {
-    const response = await fetch(baseAPI);
+    const response = await fetch(api);
     const jsonResponse = await response.json();
     const playersArray = jsonResponse.data.players;
     
@@ -23,6 +24,7 @@ const getPlayers = async () => {
     })
     
     displayPlayers();
+    console.log(state);
 
   } catch (err) {
     alert(`Sorry! There has been an error!`)
@@ -31,15 +33,19 @@ const getPlayers = async () => {
 }
 
 const displayPlayers = () => {
+  const main = document.querySelector("main");
+
+  main.innerHTML = `
+  <ol></ol>
+  `;
+
   const ol = document.querySelector("ol");
 
   for (let player in state) {
     const li = document.createElement("li");
     const name = player;
 
-    li.innerHTML = `
-    ${name}
-    `;
+    li.innerText = `${name}`;
 
     ol.append(li);
   }
@@ -48,10 +54,40 @@ const displayPlayers = () => {
 
   allPlayers.forEach(player => {
     player.addEventListener("click", (event) => {
-      console.log(event.target.innerText)
       const playerName = event.target.innerText;
+
+      showPlayerDetails(playerName);
     })
   })
 }
+
+const showPlayerDetails = playerName => {
+  const breed = state[playerName].breed;
+  const status = state[playerName].status;
+  const imageURL = state[playerName].imageUrl;
+
+  const main = document.querySelector("main");
+
+  main.innerHTML = `
+  <section>
+  <img src="${imageURL}" height=250px width=auto>
+  <h2>${playerName}</h2>
+  <ul>
+    <li>${breed}</li>
+    <li>${status}</li>
+    <br>
+    
+  <button>Go Back to Roster</button>
+  </ul>
+  </section>
+  `
+
+  const button = document.querySelector("button");
+  button.addEventListener("click", (event) => {
+    displayPlayers();
+  })
+}
+
+
 
 getPlayers();
